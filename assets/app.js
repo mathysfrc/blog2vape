1b8d3fabb84a5843880dd26cd1d570c4279da269
mathysfrc <mathysf41@gmail.com>
2023-07-11 17:30:08 +0200

    modifs init

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+
+require ('bootstrap');
+
+import 'bootstrap/dist/css/bootstrap.min.css';
